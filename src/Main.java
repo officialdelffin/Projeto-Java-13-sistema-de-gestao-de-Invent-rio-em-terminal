f425f57f8a9a3f16f4b28255f0794cbd8d323523
officialdelffin <workdelffin@gmail.com>
2026-02-06 20:56:44 -0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+/* Projeto : Sistema de Gestão de Inventário em Terminal
+
+Projeto em Java focado na aplicação prática de Orientação a Objetos. O sistema organiza o cadastro de produtos e itens perecíveis através de herança e polimorfismo. Utiliza listas dinâmicas com ArrayList,
+ sobrescrita de métodos e construtores personalizados para garantir que cada objeto seja inicializado com integridade técnica.*/
 
 
+// Classe principal do sistema :
 public class Main {
 
 
+    // Executa o sistema principal :
     public static void main ( String[] args ) {
 
 
---
+++ src/br/com/officialdelffin/AbstractClass/Product.java
@@ -0,0 +1,59 @@
+
+// Pacotes :
+package br.com.officialdelffin.AbstractClass;
+
+
+// Classe abstrata de produtos :
+public class Product {
+
+
+    // Atributos :
+    private String name;
+    private double price ;
+    private int amount ;
+
+
+    // Metodos Getters e Setters :
+
+    // Getters :
+    public String getName() {
+
+
+        return name;
+
+
+    }
+
+    public double getPrice() {
+
+
+        return price;
+
+
+    }
+
+
+    public int getAmount() {
+
+
+        return amount;
+
+
+    }
+
+
+    // Construtores :
+
+
+    // Faz com que obrige ao criar o objeto passar os parametros name, price , amount :
+    public Product ( String name , double price , int amount ) {
+
+
+        this.name    = name;
+        this.price   = price;
+        this.amount  = amount;
+
+
+    }
+
+}
