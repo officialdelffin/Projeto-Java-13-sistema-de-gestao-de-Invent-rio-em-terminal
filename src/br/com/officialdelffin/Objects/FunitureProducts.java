ec6503c452fe9f865e179674c0ea7768601d3ba5
officialdelffin <workdelffin@gmail.com>
2026-02-09 12:34:49 -0300

    commit 9

--- src/br/com/officialdelffin/Objects/FunitureProducts.java
+++ src/br/com/officialdelffin/Objects/FunitureProducts.java
@@ -4,11 +4,27 @@
 package br.com.officialdelffin.Objects;
 
 
+import br.com.officialdelffin.AbstractClass.Product;
+
 // Classe responsável por criar objetos de mobilia :
-public class FunitureProducts {
+public class FunitureProducts extends Product {
 
 
+    // Atributos :
+    private int warrantyPeriod;
 
+
+    // Construtores :
+
+    public FunitureProducts(String name, double price, int amount, int warrantyPeriod) {
+
+
+        // Atribuindo parametros com os atributos :
+        super(name,price,amount);
+        this.warrantyPeriod = warrantyPeriod;
+
+
+    }
 
 
 }
